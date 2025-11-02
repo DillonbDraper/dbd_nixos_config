fdaf7549a25686df37c7e7ae3fb4f459bb630ebf
Dillon Draper <dillonbdraper@gmail.com>
2025-11-02 14:12:27 -0600

    Moved laptop specific hardware config into proper laptop specific domain

--- hosts/roy/configuration.nix
+++ hosts/roy/configuration.nix
@@ -6,6 +6,17 @@
     ./hardware-configuration.nix
   ];
 
+  hardware.nvidia.prime = {
+  sync.enable = true;
+
+  # Enable for lesser power consumption but worse performance
+  # sync.offload = true;
+
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:1:0:0";
+  };
+
+
   networking.hostName = "roy";
 }
 
